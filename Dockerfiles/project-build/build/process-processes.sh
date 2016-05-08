767ab3809a22d2f422d5fb37ba43568c7ae0d9f6
Frank Pommerening <frank@pommerening-online.de>
2016-05-08 20:25:38 +0200

    fix build scripts

--- Dockerfiles/project-build/build/process-processes.sh
+++ Dockerfiles/project-build/build/process-processes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir temp
-cp -r /projects/MicroserviceRabbitMQ/Samples/ProcessChain/ /build/temp/app/
+cp -r /projects/MicroserviceRabbitMQ/ProcessChain/ /build/temp/app/
 cp /projects/MicroserviceRabbitMQ/Dockerfiles/process-processes/Dockerfile /build/temp/
 cd /build/temp/
 docker build -t fpommerening/msrmq:process-processes .
