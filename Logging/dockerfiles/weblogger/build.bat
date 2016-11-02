926bf75f21f543ce288fc115f2a90920922a5fec
Frank Pommerening <frank@pommerening-online.de>
2016-11-02 08:40:44 +0100

    Logging: fix dockerfiles +build

--- Logging/dockerfiles/weblogger/build.bat
+++ Logging/dockerfiles/weblogger/build.bat
@@ -5,17 +5,17 @@ cd c:\temp\msrmq
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\logging-caller\app
 copy c:\temp\msrmq\logging-caller.7z
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\logging-caller
-rem docker build -t fpommerening/msrmq:logging-caller .
+docker build -t fpommerening/msrmq:logging-caller .
 
 
-cd C:\Projects\MicroserviceRabbitMQ\Logging\src\ConsoleListener
+cd C:\Projects\MicroserviceRabbitMQ\Logging\src\Console
 dotnet publish -c Release -o c:\temp\msrmq\logging-console
 cd c:\temp\msrmq
 "c:\Program Files\7-Zip"\7z a logging-console.7z c:\temp\msrmq\logging-console
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\logging-console\app
 copy c:\temp\msrmq\logging-console.7z
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\logging-console
-rem docker build -t fpommerening/msrmq:logging-console .
+docker build -t fpommerening/msrmq:logging-console .
 
 
 cd C:\Projects\MicroserviceRabbitMQ\Logging\src\WebLogger
@@ -25,7 +25,7 @@ cd c:\temp\msrmq
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\logging-web\app
 copy c:\temp\msrmq\logging-web.7z
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\logging-web
-rem docker build -t fpommerening/msrmq:logging-web .
+docker build -t fpommerening/msrmq:logging-web .
 
 
 cd C:\Projects\MicroserviceRabbitMQ\Logging\dockerfiles\weblogger
