fe85e53c9d572b885db891eb097a720af2dbdaee
Frank Pommerening <frank@pommerening-online.de>
2017-05-10 22:44:38 +0200

    Update user

--- PicFlow/dockerfiles/picflow-database/init.sql
+++ PicFlow/dockerfiles/picflow-database/init.sql
@@ -13,23 +13,16 @@ CREATE TABLE "mt_doc_user"
 COMMENT ON TABLE "mt_doc_user"
     IS 'origin:Marten.IDocumentStore, Marten, Version=1.1.0.762, Culture=neutral, PublicKeyToken=null';
 
-INSERT INTO "mt_doc_user"(id, data, mt_last_modified, mt_version, mt_dotnet_type)
-	values ('3e149ba6-6e45-4057-8c9e-17be9f426438',
-'{"UserName": "jmeier", "LastName": "Meier", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY=", "Id": "3e149ba6-6e45-4057-8c9e-17be9f426334", "FirstName": "Jürgen"}',
- '2016-10-10 19:39:16.424395+00', '3e149ba6-a945-4e98-93b6-213e746bf64f', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User');
-	
-INSERT INTO "mt_doc_user"(id, data, mt_last_modified, mt_version, mt_dotnet_type)
-	values ('47149ba6-1b42-4a27-8e8d-6ea09e88d43f',
-'{"UserName": "wkaufmann", "LastName": "Kaufmann", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY=", "Id" : "47149ba6-1b42-4a27-8e8d-6ea09e88d43f", "FirstName": "Wolfgang"}',
- '2016-10-10 19:41:16.480185+00', '47149ba6-5a42-4eab-b96a-76de6cac7c3a', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User');
+                                                                                                                                         
+INSERT INTO mt_doc_user (id, data, mt_last_modified, mt_version, mt_dotnet_type) VALUES ('7b5e9972-4f2c-4a36-9929-c37f66846a45', 
+'{"Id": "7b5e9972-4f2c-4a36-9929-c37f66846a45", "LastName": "Maier", "UserName": "jmaier", "FirstName": "Jürgen", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY="}',
+ '2017-05-10 18:35:57.767141+00', '28136fa7-0e42-4fe8-81ce-ef4a4235d143', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User');      
 
-INSERT INTO "mt_doc_user"(id, data, mt_last_modified, mt_version, mt_dotnet_type)
-	values ('4c149ba6-974e-4996-8fe3-c0a849f53e09',
-'{"UserName": "smueller", "LastName": "Müller", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY=", "Id" : "4c149ba6-974e-4996-8fe3-c0a849f53e09", "FirstName": "Sabine"}',
- '2016-10-10 19:42:35.391597+00', '4c149ba6-d34e-430d-9b2a-4d7703e5208d', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User');
- 
- INSERT INTO "mt_doc_user"(id, data, mt_last_modified, mt_version, mt_dotnet_type)
-	values ('4f149ba6-a147-4191-ae23-f67614dd50b1',
-'{"UserName": "klehmann", "LastName": "Lehmann", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY=", "Id" : "4f149ba6-a147-4191-ae23-f67614dd50b1", "FirstName": "Kevin"}',
- '2016-10-10 19:43:10.415857+00', '4f149ba6-df47-477a-9719-30e0aab49b81', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User');
-	
+INSERT INTO mt_doc_user (id, data, mt_last_modified, mt_version, mt_dotnet_type) VALUES ('b44af49a-b1e9-471d-8f40-22bde0f27440',
+'{"Id": "b44af49a-b1e9-471d-8f40-22bde0f27440", "LastName": "Kaufmann", "UserName": "wkaufmann", "FirstName": "Wolfgang", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY="}',
+ '2017-05-10 18:37:41.280775+00', '2f136fa7-5f4b-4cb8-b5ca-085390e944ce', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User'
+);                                                                                                                                       
+INSERT INTO mt_doc_user (id, data, mt_last_modified, mt_version, mt_dotnet_type) VALUES ('e4edb4db-e918-47f2-afe6-6e41eff35ae0',
+'{"Id": "e4edb4db-e918-47f2-afe6-6e41eff35ae0", "LastName": "Mustermann", "UserName": "mmustermann", "FirstName": "Max", "PasswordHash": "tPOxK2RU/2OsmXbei9RZFPaROVY="}',
+ '2017-05-10 18:41:19.653531+00', '3f136fa7-474b-4698-8afd-46ab459dc0c1', 'FP.MsRMQ.PicFlow.Contracts.Dbo.User'
+ );
