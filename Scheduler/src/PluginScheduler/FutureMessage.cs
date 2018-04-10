218e8ccd9586c9b825d5eb4a1905647a2609254c
Frank Pommerening <frank@pommerening-online.de>
2018-04-10 21:51:18 +0200

    Add PluginScheduler sample

---
+++ Scheduler/src/PluginScheduler/FutureMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FP.MsRmq.PluginScheduler
+{
+    public class FutureMessage
+    {
+        public FutureMessage()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ Scheduler/src/PluginScheduler/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using EasyNetQ;
+using EasyNetQ.Scheduling;
+
+namespace FP.MsRmq.PluginScheduler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IBus myBus = null;
+            try
+            {
+                myBus = RabbitHutch.CreateBus("host=localhost", 
+                    register => register.Register<IScheduler, DelayedExchangeScheduler>() );
+                StartSubscription(myBus);
+                ScheduleMessage(myBus);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                myBus?.Dispose();
+            }
+            Console.ReadLine();
+        }
+
+        private static void StartSubscription(IBus myBus)
+        {
+            myBus.Subscribe<FutureMessage>("MyFutureMessageSubscription", msg => Console.WriteLine($"{DateTime.Now:T}: {msg.Content} (schedule at {msg.Timestamp:T})"));
+           
+        }
+
+        private static void ScheduleMessage(IBus myBus)
+        {
+            string content = string.Empty;
+            do
+            {
+                Console.WriteLine("Enter the content or nothing to leave");
+                content = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(content))
+                {
+                    Console.WriteLine("Enter the delay in seconds");
+                    var delayInSeconds = Convert.ToInt32(Console.ReadLine());
+
+                    myBus.FuturePublish(TimeSpan.FromSeconds(delayInSeconds), new FutureMessage { Content = content });
+
+                }
+                System.Threading.Thread.Sleep(1000);
+            } while (!string.IsNullOrEmpty(content));
+
+        }
+    }
+}
